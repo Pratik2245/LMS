36797b9e762ff1b9517d0e08a4d1dab4d4d0c193
Pratik2245 <paithankarpratik3@gmail.com>
2025-02-15 23:43:04 +0530

    navbar done and authentication added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useMatch } from "react-router-dom";
 import Home from "./pages/students/Home";
 import CourseList from "./pages/students/CourseList";
 import CoursesDetails from "./pages/students/CoursesDetails";
@@ -10,15 +10,20 @@ import Dashboard from "./pages/educators/Dashboard";
 import MyCourses from "./pages/educators/MyCourses";
 import StudentsEnrolled from "./pages/educators/StudentsEnrolled";
 import AddCourses from "./pages/educators/AddCourses";
+import Navbar from "./components/students/Navbar";
+import MyEnrollments from "./pages/students/MyEnrollments";
 
 const App = () => {
+  const isEducatorRoute = useMatch("/educator/*");
   return (
-    <div>
+    <div className="text-default min-h-screen bg-white">
+      {!isEducatorRoute && <Navbar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course-list" element={<CourseList />} />
         <Route path="/course-list/:input" element={<CourseList />} />
         <Route path="/course/:id" element={<CoursesDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
         <Route path="/educator" element={<Educator />}>
--- frontend/src/components/students/Navbar.jsx
+++ frontend/src/components/students/Navbar.jsx
@@ -1,14 +1,64 @@
-import React from "react";
 import { assets } from "../../assets/assets";
-
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
+import { Link, Links } from "react-router-dom";
 const Navbar = () => {
+  const isCoursePageList = location.pathname.includes("/course-list");
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
+  console.log(user);
+
   return (
-    <div>
+    <div
+      className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
+        isCoursePageList ? "bg-white" : "bg-cyan-100/70"
+      }`}
+    >
       <img
         src={assets.logo}
         alt="navbarImage"
         className="w-28 lg:w-32 cursor-pointer"
       />
+      <div className="hidden md:flex items-center gap-5 text-gray-500">
+        <div className="flex items-center gap-5">
+          {user && (
+            <>
+              <button>Become Educator</button>
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
+        </div>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={() => openSignIn()}
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+          >
+            Create Account
+          </button>
+        )}
+      </div>
+      {/* for phone screens  */}
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+        <div className="flex items-center gap-2">
+          {user && (
+            <>
+              <button className="px-3 py-2">Become Educator</button>
+              <span>|</span>
+              <Link to={"/my-enrollments"} className="px-4 py-2">
+                My Enrollments
+              </Link>
+            </>
+          )}
+          {user ? (
+            <UserButton />
+          ) : (
+            <button onClick={() => openSignIn()} className="p-2">
+              <img src={assets.user_icon} alt="User Icon" className="w-8 h-8" />
+            </button>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
